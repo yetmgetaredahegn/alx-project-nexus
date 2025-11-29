77c6038daef805b4be5fd4a93cbcf76ab727e586
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-11-30 00:14:16 +0300

    fix: email configs issues

--- alx_project_nexus/settings/production.py
+++ alx_project_nexus/settings/production.py
@@ -85,8 +85,8 @@ CELERY_TASK_ALWAYS_EAGER = False
 
 # Email Backend - Use SMTP for production
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp-relay.brevo.com")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "sandbox.smtp.mailtrap.io")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 2525))
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
